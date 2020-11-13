3c9a58259fd841d8dc05966ec9cd21af7e6d17cb
linus <pjlintw@gmail.com>
2020-11-13 21:35:45 +0100

    build data

---
+++ assignment1/build_zipf_data.py
@@ -0,0 +1,24 @@
+"""Precessing, build vocabulary and data."""
+
+from utils import get_args, load_config
+
+
+def preprocess(lower_case=False):
+    pass
+
+def main():
+    # config
+    args = get_args()
+    config = load_config(args)
+
+    # aceesss config
+
+    # preprocess
+
+    # build vocabulary with frequency
+
+    # output
+    pass
+
+if __name__ == "__main__":
+    main()
