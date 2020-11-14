17e4d479203d10ccc0c197f986d44e50ca65c7b0
linus <pjlintw@gmail.com>
2020-11-14 11:33:35 +0100

    read file

--- assignment1/build_zipf_data.py
+++ assignment1/build_zipf_data.py
@@ -6,12 +6,25 @@ from utils import get_args, load_config
 def preprocess(lower_case=False):
     pass
 
+
 def main():
     # config
     args = get_args()
     config = load_config(args)
 
     # aceesss config
+    corpus_lst = config['vocab']['corpus']
+    for corpus_path in corpus_lst:
+        print(corpus_path)
+        
+        # check path is valid
+        corpus = list()
+        with open(corpus_path, 'r', encoding='utf-8') as f:
+            print(repr(f.readlines()))
+            corpus.extend(f.readlines())
+            print(corpus)
+
+
 
     # preprocess
 
