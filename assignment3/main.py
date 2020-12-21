a241449758b4afca4f80a696f3dacb2a1836a54e
linus <pjlintw@gmail.com>
2020-12-21 23:16:48 +0100

    update

--- assignment3/main.py
+++ assignment3/main.py
@@ -1,8 +1,31 @@
 
 
+class CKYRecognizer():
+    def __init__(self, grammar):
+        super.__init__()
+
+    def recognize(self, tokens):
+        """Check if the given input is in language
+    
+        1. unknown words
+        2. can not derive to `S` 
+        """
+        return None
+
+    def parse(self):
+        return None
 
 def main():
-    pass
+    import nltk
+    from nltk.tree import Tree
+
+
+    grammar = nltk.data.load("grammars/atis-grammar-cnf.cfg") 
+    grammar = nltk.data.load("atis//atis.cfg") 
+    #cng_grammar = grammar.chomsky_normal_form()
+
+    print(type(cng_grammar))
+
 
 
 if __name__ == '__main__':
