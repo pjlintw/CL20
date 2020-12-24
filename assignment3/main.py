54b232ade6ebf07311fff87591105891e43fa40a
linus <pjlintw@gmail.com>
2020-12-24 23:49:25 +0100

    toy example

--- assignment3/main.py
+++ assignment3/main.py
@@ -15,16 +15,33 @@ class CKYRecognizer():
     def parse(self):
         return None
 
+def load_data(path):
+    with open(path) as f:
+        return f.readlines()
+
 def main():
     import nltk
     from nltk.tree import Tree
 
 
-    grammar = nltk.data.load("grammars/atis-grammar-cnf.cfg") 
-    grammar = nltk.data.load("atis//atis.cfg") 
+    #grammar = nltk.data.load("grammars/atis-grammar-cnf.cfg") 
+    # grammar = nltk.data.load("atis/atis-grammar-cnf.cfg") 
     #cng_grammar = grammar.chomsky_normal_form()
 
-    print(type(cng_grammar))
+    #print(grammar)
+
+    ###
+    # toy
+    ###
+    grammar_lst = load_data('toy_data/toy-grammar.txt')
+    grammar_set = set([item.strip() for item in grammar_lst])
+    print('S -> NP VP'  in grammar_set)
+    print('NP -> Det N' in grammar_set)
+    print('VP -> V NP' in grammar_set)
+    print('V -> ate' in grammar_set)
+    print('NP -> john' in grammar_set)
+    print('Det -> a' in grammar_set)
+    print('N -> sandwich' in grammar_set)
 
 
 
