35ef4bd58b828c7c1b0ad2046f55ed09221acd02
linus <pjlintw@gmail.com>
2020-12-29 14:39:47 +0100

    modify last dimension as list

--- assignment3/main.py
+++ assignment3/main.py
@@ -8,6 +8,8 @@ Make sure the production rules file is in CNF.
 import pprint
 from collections import defaultdict
 import numpy as np
+from nltk import Tree, ImmutableTree
+import time
 
 pp = pprint.PrettyPrinter(depth=4)
 
@@ -126,14 +128,32 @@ def parse_fn(sentences):
         yield _parse(sent)
 
 
-def backtracer(chart):
-    """Backtracing the parsing trees from given chart.
+def check_coverage(tokens, grammar):
+    """Check grammar covers token.
+
+    """
+    cover = [True for t in tokens if t not in grammar ]
+    return None
+    
+
+def backtracer(root_symbol, chart, i, j):
+    """Backtracing all parsing trees from given chart.
 
     Args:
-      chart: ...
+      symbol: SIGMA
     Returns:
     """
-    return None
+    trees = list()
+    if type(chart[i][j][root_symbol][0]) == str:
+        return [root_symbol, chart[i][j][root_symbol]]
+    else:
+        for prod in chart[i][j][root_symbol]:
+            jointed_str, split = prod
+            lhs, rhs = jointed_str.split('|')
+            left = backtracer(lhs, chart, i, split)
+            right = backtracer(rhs, chart, split,j)
+            return [root_symbol, left, right]
+                    
 
 
 def recognize(sent, grammar):
@@ -153,12 +173,17 @@ def recognize(sent, grammar):
     # Chart(i, k) with shape (n+1, n+1)
     # Each cell in chart[i][k] keeps one or more than one non-terminal symbol representing a substring
     # sentence[i][k] represents a list of substring from i to k-1.
-    chart = [ [ defaultdict(lambda: list()) for _ in range(sent_len+1)] for _ in range(sent_len+1)]
+    
+    #chart = [ [ defaultdict(lambda: list()) for _ in range(sent_len+1)] for _ in range(sent_len+1)]
+    chart = [ [ list() for _ in range(sent_len+1)] for _ in range(sent_len+1)]
+    # chart = [ [ set() for _ in range(sent_len+1)] for _ in range(sent_len+1)]
+
     # span 1: (0,1), (1,2), ..., (n-1,n) 
     # span 2: (0,2), (1,3), ..., (n-2,n)
     # span n, (0,n)
 
     ### labels terminal symbols (token) with chart ###
+    cl = list()
     for i in range(sent_len):
         tok = sent[i]
         if tok in grammar:
@@ -166,15 +191,18 @@ def recognize(sent, grammar):
             nonterms = grammar[tok]
             # key(str): [token(str)]
             for nonterm in nonterms:
-                chart[i][i+1][nonterm].append(tok)
-            
-            print('span 1', sent[i:i+1])
-            pprint.pprint(dict(chart[i][i+1]))
-            print()
+
+                # chart[i][i+1][nonterm].append(tok)          
+                # chart[i][i+1].append(Tree(nonterm, [tok]))
+                chart[i][i+1].extend([ImmutableTree(nonterm, [tok])])
+                # chart[i][i+1].add(ImmutableTree(nonterm, [tok, i]))
+
+            # print('span 1', sent[i:i+1])
+            # pprint.pprint(dict(chart[i][i+1]))
+            # print()
         else:
             sys_info = f'token {tok} not in grammar'
             return isInLanguage, sys_info
-
     
     ### labels substring with length from 2 to sentence length ###
     # span 2: [john|ate](0:1+1), [ate|a](1:2+1), [a|sandwitch](3:3+1)
@@ -183,39 +211,37 @@ def recognize(sent, grammar):
         for begin in range(0, sent_len-span+1):
             end = begin + span
             for split in range(begin+1, end):
-                print(f'span:{span}, begin:{begin}, split:{split}, end:{end}')
-                print(f'substrings:{sent[begin:split]}, {sent[split:end]}')
+                # print(f'span:{span}, begin:{begin}, split:{split}, end:{end}')
+                # print(f'substrings:{sent[begin:split]}, {sent[split:end]}')
                 
-                left_nonterms = chart[begin][split]
-                right_nonterms = chart[split][end]
+                # get label
+                left_nonterms = [ ele.label() for ele in chart[begin][split] ]
+                right_nonterms =[ ele.label() for ele in chart[split][end] ]
 
-                ### check ###
-                # chart[i][j][A] = ['token'] == sent[i:j]
-                if span == 2:
-                    tok = sent[begin:end]
-                    #print('span 2, substring', tok)
-                    #print_defaultdict(left_nonterms)
-                    #print()
-                    for k in left_nonterms:
-                        cur_tok = sent[begin:split]
-                        #print('current token',cur_tok)
-                        #print(cur_tok == left_nonterms[k])
-                        assert cur_tok == left_nonterms[k]
-                ### check ###
-                   
+                # print(left_nonterms)
+                # print(right_nonterms)
+
                 ### EXTRAX: ###
                 # group subnodes and split k as tuple of (B|C, k)
                 # add tuple to corresponding parent, if (B, C) as children of substree
                 for B in left_nonterms:
                     for C in right_nonterms:
                         children = '|'.join([B, C])
+                        
                         # print('each BC', B,C)
                         # print('B C in grammar', children in grammar) 
                         if children in grammar:
                             parents = grammar[children]
                             for parent in parents:
                                 tup = (children, split)
-                                chart[begin][end][parent].append(tup)
+
+                                # chart[begin][end][parent].append(tup)
+                                # chart[begin][end].append(Tree(parent ,[B, C, split]))
+                                chart[begin][end].extend([ImmutableTree(parent, [B, C, split])])
+                                # chart[begin][end].add(ImmutableTree(parent, [B, C, span, begin, split]))
+
+                                
+
                                 #print('index of begin and end', begin, end)
                             # print(sent[begin:end])
                             # print(dict(chart[begin][end]))
@@ -237,37 +263,33 @@ def recognize(sent, grammar):
                 #         print(chart[begin][end][k])
                 # print()
                 
-            pp.pprint(dict(chart[begin][end]))
-            print()
-        #print()
-    # pp.pprint(chart[0][4])
+            #pp.pprint(dict(chart[begin][end]))
+            #print()
 
-    #trees, num_tree = backtracer(chart)
-
-    print('end', end)
     roots = chart[0][end]
-    if 'SIGMA' in roots:
-        print('SIGMA' in roots) 
-        print(roots['SIGMA'])
-        print(len(roots['SIGMA']))
-        print(len(roots['S']))
-        isInLanguage = True
-        return isInLanguage, sys_info
+    #pp.pprint(roots)
+
+
+    trees = [t for t in roots if t.label() == 'SIGMA']
+    # print(trees)
+
+    n_tree = 0
+    if trees:
+        n_tree = len(trees)
+        print('number of tree', n_tree)
+
+        # 
+        # t = backtracer('SIGMA', chart, 0, sent_len)
+        # print(t)
+        # print(t[1:])
+        # sentence = Tree('SIGMA', t[1:]) 
+        # sentence.draw()
+        # isInLanguage = True
+        # return isInLanguage, sys_info
 
     return isInLanguage, sys_info
 
-def printTable(myDict, colList=None):
-   """ Pretty print a list of dictionaries (myDict) as a dynamically sized table.
-   If column names (colList) aren't specified, they will show in random order.
-   Author: Thierry Husson - Use it as you want but don't blame me.
-   """
-   if not colList: colList = list(myDict[0].keys() if myDict else [])
-   myList = [colList] # 1st row = header
-   for item in myDict: myList.append([str(item[col] if item[col] is not None else '') for col in colList])
-   colSize = [max(map(len,col)) for col in zip(*myList)]
-   formatStr = ' | '.join(["{{:<{}}}".format(i) for i in colSize])
-   myList.insert(1, ['-' * i for i in colSize]) # Seperating line
-   for item in myList: print(formatStr.format(*item))
+
 
 def main():
     import nltk
@@ -277,8 +299,10 @@ def main():
     toy_grammar_file = 'toy_data/toy-grammar.txt'
     toy_grammar_file2 = 'toy_data/atis-grammar-cnf.cfg'
 
+
     toy_sent_file = 'toy_data/toy-sentences.txt'
-    toy_one_sent_file = 'toy_data/toy-one_sentence.txt'
+    toy_one_sent_file = 'toy_data/toy-one-sentence.txt'
+    toy_five_sent_file = 'toy_data/toy-5-sentence.txt'
 
     #grammar = nltk.data.load("grammars/atis-grammar-cnf.cfg") 
     # grammar = nltk.data.load("atis/atis-grammar-cnf.cfg") 
@@ -303,8 +327,12 @@ def main():
 
 
     ### recognize sentence ###
-    test_sent = load_data(toy_one_sent_file)
+    test_sent = load_data(toy_five_sent_file)
+    start = time.time()
     for sent in parse_fn(test_sent):
+        # check grammar covers the tokens
+        check_coverage(sent, grammar)
+
         isInLanuage, sys_info = recognize(sent, grammar)
         sent_str = ' '.join([ tok.replace('\"', '') for tok in sent])
         if isInLanuage:
@@ -313,8 +341,22 @@ def main():
             print(f'sentence, \"{sent_str}\", not in language')
             print(sys_info)
 
-    s = ['show', 'the', 'flights', '.']
+    print(time.time()-start)
 
+    t_1 = ImmutableTree('3', ['1','1'])
+    t_2 = ImmutableTree('3', ['1','2'])
+    t_3 = ImmutableTree('3', ['3','3'])
+    
+    c = set()
+    c.add(t_1)
+    print(c)
+    c.add(t_2)
+    print(c)
+    c.add(t_3)
+    print(c)
+    
+    
+    
 
     ## 2, 3, 2, 2, 2, 200
 if __name__ == '__main__':
