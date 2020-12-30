3c46eedf4a72c544bada858f5cdc29d8afbb713b
linus <pjlintw@gmail.com>
2020-12-30 15:00:31 +0100

    run cky wrapper

--- assignment3/main.py
+++ assignment3/main.py
@@ -3,12 +3,15 @@ This CKY algorithm for recognizing language and syntatic parsing.
 The parsing function designs for the grammar which has Chomsky normal (CNF) form and *don't* handle unary rule,
 i.e. all production rules are of the form A -> B C, or A -> "token".
 Make sure the production rules file is in CNF.
+
+Feature: we construct a Node object as a Tree to record the root node and subnodes.
+It allows us to backtrace all previous nonterminal node until terminal symbol.
+
+
 """
 
 import pprint
 from collections import defaultdict
-import numpy as np
-from nltk import Tree, ImmutableTree
 import time
 
 pp = pprint.PrettyPrinter(depth=4)
@@ -16,27 +19,6 @@ pp = pprint.PrettyPrinter(depth=4)
 def load_data(path):
     with open(path) as f:
         return f.readlines()
-
-
-def print_defaultdict(defaultDict):
-    """Print defaultdict with its type."""
-    print(dict.__repr__(defaultDict))
-
-
-class CKYRecognizer():
-    def __init__(self, grammar):
-        super.__init__()
-
-    def recognize(self, tokens):
-        """Check if the given input is in language
-    
-        1. unknown words
-        2. can not derive to `S` 
-        """
-        return None
-
-    def parse(self):
-        return None
 
 
 def create_product_rule(path):
@@ -65,7 +47,7 @@ def create_product_rule(path):
 
     """
     def add_rule_to_dict(rule_dict, lhs, rhs):
-        """Add production rule to (non-)terminal-rule dictionary.
+        """Add production rule to (non-)terminal rule dictionary.
 
         Args:
           rule_dict: dict, mapping terminal symbol or two nonterminal o one nonterminal
@@ -91,10 +73,8 @@ def create_product_rule(path):
     nonterminal_symbol = set()
     for line in load_data(path):
         lhs, rhs = line.split('->')
-        lhs = lhs.strip()
-        rhs = rhs.strip()
+        lhs, rhs = lhs.strip(), rhs.strip()
         rhs = [ tok for tok in rhs.split() if tok !='']
-
         # add nonterminal symbol
         nonterminal_symbol.add(lhs)
         if len(rhs) == 1:
@@ -109,15 +89,14 @@ def create_product_rule(path):
             #print(grammar)
         else:
             print('production rule not in CNF', line)
-    #print(grammar)
     return grammar, nonterminal_symbol
 
 
 def parse_fn(sentences):
-    """
+    """Preprocessing sentences of list. 
 
     Args:
-      sentences: () 
+      sentences:
     """
     def _parse(sentence):
         lower_sent = sentence.lower()
@@ -128,181 +107,174 @@ def parse_fn(sentences):
         yield _parse(sent)
 
 
-def check_coverage(tokens, grammar):
-    """Check grammar covers token.
-
-    """
-    cover = [True for t in tokens if t not in grammar ]
-    return None
-    
-
-def backtracer(root_symbol, chart, i, j):
-    """Backtracing all parsing trees from given chart.
+def run_cky_wrapper(parser, sent_lst, output='output.o'):
+    """CKY 
 
     Args:
-      symbol: SIGMA
+      parser
+      sent_lst: a list of sentence
+      output: 
     Returns:
     """
-    trees = list()
-    if type(chart[i][j][root_symbol][0]) == str:
-        return [root_symbol, chart[i][j][root_symbol]]
-    else:
-        for prod in chart[i][j][root_symbol]:
-            jointed_str, split = prod
-            lhs, rhs = jointed_str.split('|')
-            left = backtracer(lhs, chart, i, split)
-            right = backtracer(rhs, chart, split,j)
-            return [root_symbol, left, right]
+    start = time.time()
+    writer = open(output, 'w', encoding='utf-8')
+    for sent in parse_fn(sent_lst):
+        sent_len = len(sent)
+
+        # chart[0][sent_len]
+        roots = parser.parse(sent)
+        trees = [prod for prod in roots if prod._root == 'SIGMA']
+        num_trees = len(trees)
+
+        sent_str = ' '.join(sent).replace('\"', '') 
+        writer.write(f'{sent_str}\t{num_trees}\n')
+        
+        # t = backtracer(tree)
+        # t_nltk = Tree.fromstring(t)
+        # t_nltk.draw()
+    print(f'Create file to path: {output}')
+    print(f'executed {time.time()-start}')
+        
+    # sent_str = ' '.join([ tok.replace('\"', '') for tok in sent])
+    # if isInLanuage:
+    #     print(f'sentence, \"{sent_str}\", in language')
+    # else:
+    #     print(f'sentence, \"{sent_str}\", not in language')
+    #     print(sys_info)
+
+class Node:
+    """a Node class represents tree."""
+    def __init__(self, root, left, right, end):
+        """Consturct a node of substree with root, left, right,
+        and terminal symbol variable.
+
+        Args:
+          root: string, a root node of tree
+          left: string, left hand side child
+          right: string, right hand side child
+          end: string, terminal symbol (token)
+        """
+        self._root = root
+        self._left = left
+        self._right = right
+        self._terminal = end
+        
+
+class CKYParser:
+    def __init__(self, grammar):
+        super(CKYParser, self).__init__()
+        self.grammar = grammar
+        self.chart = None
+
+    @staticmethod
+    def check_coverage(self, sents, grammar):
+        """Check grammar covers token.
+
+        Args:
+          sent: tokens of list
+        Returns:
+        """
+        notCover = [1 for t in tokens if t not in grammar]
+        if notCover:
+            raise ValueError("Grammar does not cover some of the input words:")
+    
+
+    def recognize(self, sent, chart):
+        """Check if the given input is in language
+    
+        1. unknown words
+        2. can not derive to `S` 
+        """
+        if not self.chart:
+            print('chart is none')
+
+        return None
+
+    def parse(self, sent):
+        """CKY parse for a sentence with chart.
+
+        Args:
+          sent: list of tokens 
+          grammar:
+
+        Returns:
+          chart: 3D lists,
+        """
+        sent_len = len(sent)
+        # Chart with shape (n+1, n+1)
+        # Each cell in chart[i][k] keeps one or more than one non-terminal symbol representing a substring
+        # sentence[i][k] represents a list of substring from i to k-1.
+        chart = [ [ set() for _ in range(sent_len+1)] for _ in range(sent_len+1)]
+        # back_chart = [ [ defaultdict(lambda: list()) for _ in range(sent_len+1)] for _ in range(sent_len+1)]
+        
+        # span 1: (0,1), (1,2), ..., (n-1,n) 
+        # span 2: (0,2), (1,3), ..., (n-2,n)
+        # span n, (0,n)
+
+        ### labels terminal symbols (token) with chart ###
+        cl = list()
+        for i in range(sent_len):
+            tok = sent[i]
+            if tok in self.grammar:
+                # list of nonterminal
+                nonterms = self.grammar[tok]
+                # key(str): [token(str)]
+                for nonterm in nonterms:
+                    chart[i][i+1].add(Node(nonterm, None, None, tok))
                     
+        
+        ### labels substring with length from 2 to sentence length ###
+        # span 2: [john|ate](0:1+1), [ate|a](1:2+1), [a|sandwitch](3:3+1)
+        for span in range(2, sent_len+1):
+            for begin in range(0, sent_len-span+1):
+                end = begin + span
+                for split in range(begin+1, end):
+                    # print(f'span:{span}, begin:{begin}, split:{split}, end:{end}')
+                    # add Node to chart[i][k] if A->BC in grammar
+                    left_nonterms = chart[begin][split] 
+                    right_nonterms = chart[split][end] 
+                    for B in left_nonterms:
+                        for C in right_nonterms:
+                            # acess the nonterminal in string
+                            b_string = B._root
+                            c_string = C._root
+                            children = '|'.join([b_string, c_string])
+                            children = '|'.join([b_string, c_string])
+                            if children in self.grammar:
+                                parents = self.grammar[children]
+                                for parent in parents:
+                                    chart[begin][end].add(Node(parent, B, C, None))
+        self.chart = chart
+        return self.chart[0][sent_len]
 
 
-def recognize(sent, grammar):
-    """CKY recognizer for a sentence
+    def backtracer(self, root_node):
+        """Recursively backtracing a parsing tree starting from an Node object.
 
-    Args:
-      sent: list of tokens 
-      grammar:
-
-    Returns:
-      isInLanguage: boolean, if the sentence in the language
-      sys_info: string for reason why not in language
-    """
-    isInLanguage = False
-    sys_info = ''
-    sent_len = len(sent)
-    # Chart(i, k) with shape (n+1, n+1)
-    # Each cell in chart[i][k] keeps one or more than one non-terminal symbol representing a substring
-    # sentence[i][k] represents a list of substring from i to k-1.
-    
-    #chart = [ [ defaultdict(lambda: list()) for _ in range(sent_len+1)] for _ in range(sent_len+1)]
-    chart = [ [ list() for _ in range(sent_len+1)] for _ in range(sent_len+1)]
-    # chart = [ [ set() for _ in range(sent_len+1)] for _ in range(sent_len+1)]
-
-    # span 1: (0,1), (1,2), ..., (n-1,n) 
-    # span 2: (0,2), (1,3), ..., (n-2,n)
-    # span n, (0,n)
-
-    ### labels terminal symbols (token) with chart ###
-    cl = list()
-    for i in range(sent_len):
-        tok = sent[i]
-        if tok in grammar:
-            # list of nonterminal
-            nonterms = grammar[tok]
-            # key(str): [token(str)]
-            for nonterm in nonterms:
-
-                # chart[i][i+1][nonterm].append(tok)          
-                # chart[i][i+1].append(Tree(nonterm, [tok]))
-                chart[i][i+1].extend([ImmutableTree(nonterm, [tok])])
-                # chart[i][i+1].add(ImmutableTree(nonterm, [tok, i]))
-
-            # print('span 1', sent[i:i+1])
-            # pprint.pprint(dict(chart[i][i+1]))
-            # print()
+        Args:
+          root_node: Node object. 
+        Returns:
+          string, parsing tree can be construct a nltk.Tree object
+        """
+        if root_node._terminal != None:
+            return f'({root_node._root} {root_node._terminal})'
         else:
-            sys_info = f'token {tok} not in grammar'
-            return isInLanguage, sys_info
-    
-    ### labels substring with length from 2 to sentence length ###
-    # span 2: [john|ate](0:1+1), [ate|a](1:2+1), [a|sandwitch](3:3+1)
-    # span 2: [john|ate a], [john ate| a]
-    for span in range(2, sent_len+1):
-        for begin in range(0, sent_len-span+1):
-            end = begin + span
-            for split in range(begin+1, end):
-                # print(f'span:{span}, begin:{begin}, split:{split}, end:{end}')
-                # print(f'substrings:{sent[begin:split]}, {sent[split:end]}')
-                
-                # get label
-                left_nonterms = [ ele.label() for ele in chart[begin][split] ]
-                right_nonterms =[ ele.label() for ele in chart[split][end] ]
-
-                # print(left_nonterms)
-                # print(right_nonterms)
-
-                ### EXTRAX: ###
-                # group subnodes and split k as tuple of (B|C, k)
-                # add tuple to corresponding parent, if (B, C) as children of substree
-                for B in left_nonterms:
-                    for C in right_nonterms:
-                        children = '|'.join([B, C])
-                        
-                        # print('each BC', B,C)
-                        # print('B C in grammar', children in grammar) 
-                        if children in grammar:
-                            parents = grammar[children]
-                            for parent in parents:
-                                tup = (children, split)
-
-                                # chart[begin][end][parent].append(tup)
-                                # chart[begin][end].append(Tree(parent ,[B, C, split]))
-                                chart[begin][end].extend([ImmutableTree(parent, [B, C, split])])
-                                # chart[begin][end].add(ImmutableTree(parent, [B, C, span, begin, split]))
-
-                                
-
-                                #print('index of begin and end', begin, end)
-                            # print(sent[begin:end])
-                            # print(dict(chart[begin][end]))
-                            # print()
-
-                left_subs = sent[begin:split]
-                right_subs = sent[split:end]
-                # print(left_subs, right_subs)
-                # print('chart[i][j]', end='\t')
-                # print_defaultdict(chart[begin][end])
-                
-                ### print A-> BC, A-DE ###
-                # for k in chart[begin][end]:
-                #     num_sub = len(chart[begin][end][k])
-                #     if num_sub > 1:
-                #         print('num_sub greater than 1')
-                #         print(begin, end)
-                #         print('key', k)
-                #         print(chart[begin][end][k])
-                # print()
-                
-            #pp.pprint(dict(chart[begin][end]))
-            #print()
-
-    roots = chart[0][end]
-    #pp.pprint(roots)
-
-
-    trees = [t for t in roots if t.label() == 'SIGMA']
-    # print(trees)
-
-    n_tree = 0
-    if trees:
-        n_tree = len(trees)
-        print('number of tree', n_tree)
-
-        # 
-        # t = backtracer('SIGMA', chart, 0, sent_len)
-        # print(t)
-        # print(t[1:])
-        # sentence = Tree('SIGMA', t[1:]) 
-        # sentence.draw()
-        # isInLanguage = True
-        # return isInLanguage, sys_info
-
-    return isInLanguage, sys_info
-
+            lhs_node, rhs_node = root_node._left, root_node._right
+            lhs = self.backtracer(lhs_node)
+            rhs = self.backtracer(rhs_node)
+            return f'({root_node._root} {lhs} {rhs})'
 
 
 def main():
-    import nltk
-    from nltk.tree import Tree
-
     grammar_file = 'atis/atis-grammar-cnf.cfg'
     toy_grammar_file = 'toy_data/toy-grammar.txt'
     toy_grammar_file2 = 'toy_data/atis-grammar-cnf.cfg'
 
-
     toy_sent_file = 'toy_data/toy-sentences.txt'
     toy_one_sent_file = 'toy_data/toy-one-sentence.txt'
     toy_five_sent_file = 'toy_data/toy-5-sentence.txt'
+
+    sent_file = 'atis/atis-test-sentences.txt'
 
     #grammar = nltk.data.load("grammars/atis-grammar-cnf.cfg") 
     # grammar = nltk.data.load("atis/atis-grammar-cnf.cfg") 
@@ -314,50 +286,22 @@ def main():
     # toy example
     ###
     grammar, _ = create_product_rule(grammar_file)
-    print('show', grammar['\"show\"'])
-    print('the', grammar['\"the\"'])
-    print('flights', grammar['\"flights\"'])
-    print('.', grammar['\".\"'])
+
+    #
+    parser = CKYParser(grammar)
+    #
 
     num_grammar = 0
     for k in grammar:
         num_grammar += len(grammar[k])
     print('num grammar', num_grammar)
     print()
-
-
-    ### recognize sentence ###
     test_sent = load_data(toy_five_sent_file)
-    start = time.time()
-    for sent in parse_fn(test_sent):
-        # check grammar covers the tokens
-        check_coverage(sent, grammar)
-
-        isInLanuage, sys_info = recognize(sent, grammar)
-        sent_str = ' '.join([ tok.replace('\"', '') for tok in sent])
-        if isInLanuage:
-            print(f'sentence, \"{sent_str}\", in language')
-        else:
-            print(f'sentence, \"{sent_str}\", not in language')
-            print(sys_info)
-
-    print(time.time()-start)
-
-    t_1 = ImmutableTree('3', ['1','1'])
-    t_2 = ImmutableTree('3', ['1','2'])
-    t_3 = ImmutableTree('3', ['3','3'])
     
-    c = set()
-    c.add(t_1)
-    print(c)
-    c.add(t_2)
-    print(c)
-    c.add(t_3)
-    print(c)
-    
-    
-    
+    # write sentence and its number of trees to file 
+    # the parser will recognize sentence if not in grammar or in lanugage
+    run_cky_wrapper(parser, test_sent)
 
-    ## 2, 3, 2, 2, 2, 200
+    
 if __name__ == '__main__':
     main()
