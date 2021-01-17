50451464a517ff701b429316a314b7d7115c6a7b
linus <pjlintw@gmail.com>
2021-01-17 23:23:54 +0100

    update

---
+++ assignment5/main.py
@@ -0,0 +1,17 @@
+"""Latent Dirichlet Allocation using Gibbs sampling algorithm."""
+
+
+
+
+
+def run_lda():
+    return None
+
+def main():
+    
+    run_lda()
+
+if __name__ == '__main__':
+    main()
+
+
